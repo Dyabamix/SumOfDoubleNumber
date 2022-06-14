e73f94d0aa64297535afca74502115964214a654
serega <numizmat161@inbox.ru>
2022-06-14 22:55:41 +0300

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Objects;
+import java.util.Optional;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,25 +12,50 @@ public class Main {
         Pair<Integer, String> pair2 = Pair.of(1, "hello");
         boolean mustBeTrue = pair.equals(pair2); // true!
         boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
-
+        System.out.println("int = " + i);
+        System.out.println("string = " + s);
+        System.out.println("pair1 =? pair2: " + mustBeTrue);
+        System.out.println("hashcode1 =? hashcode2: " + mustAlsoBeTrue);
     }
 
 }
 
 class Pair <E , H> {
+    private final E first;
+    private final H second;
 
-    private Pair() {
+    private Pair(E first, H second) {
+        this.first = first;
+        this.second = second;
     }
 
     public E getFirst(){
-        return null;
+        return this.first;
     }
 
     public H getSecond(){
-        return null;
+        return this.second;
     }
 
-    public static void of(){
+    public static <E , H> Pair<E , H> of(E first, H second){
+        return new Pair<>(first, second);
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+
+        if (!Objects.equals(first, pair.first)) return false;
+        return Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (second != null ? second.hashCode() : 0);
+        return result;
     }
 }
