bb5159e38e8618dad6cd1a6576583e448dadf4f5
serega <numizmat161@inbox.ru>
2022-06-15 23:59:21 +0300

    ok1

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,23 @@
-import java.util.Objects;
-import java.util.Optional;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 
 public class Main {
-
     public static void main(String[] args) {
+        String strTest1 = "Мама мыла-мыла-мыла раму!";
 
-        Pair<Integer, String> pair = Pair.of(1, "hello");
-        Integer i = pair.getFirst(); // 1
-        String s = pair.getSecond(); // "hello"
+        String strTest2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales consectetur purus at faucibus. Donec mi quam, tempor vel ipsum non, faucibus suscipit massa. Morbi lacinia velit blandit tincidunt efficitur. Vestibulum eget metus imperdiet sapien laoreet faucibus. Nunc eget vehicula mauris, ac auctor lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel odio nec mi tempor dignissim.";
 
-        Pair<Integer, String> pair2 = Pair.of(1, "hello");
-        boolean mustBeTrue = pair.equals(pair2); // true!
-        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
-        System.out.println("int = " + i);
-        System.out.println("string = " + s);
-        System.out.println("pair1 =? pair2: " + mustBeTrue);
-        System.out.println("hashcode1 =? hashcode2: " + mustAlsoBeTrue);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(strTest1.getBytes()), StandardCharsets.UTF_8));
+
+        Scanner scanner = new Scanner(bf);
+        List<String> topWord = Stream.generate(scanner.tokens())
     }
 
-}
 
-class Pair <E , H> {
-    private final E first;
-    private final H second;
-
-    private Pair(E first, H second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    public E getFirst(){
-        return this.first;
-    }
-
-    public H getSecond(){
-        return this.second;
-    }
-
-    public static <E , H> Pair<E , H> of(E first, H second){
-        return new Pair<>(first, second);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Pair<?, ?> pair = (Pair<?, ?>) o;
-
-        if (!Objects.equals(first, pair.first)) return false;
-        return Objects.equals(second, pair.second);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = first != null ? first.hashCode() : 0;
-        result = 31 * result + (second != null ? second.hashCode() : 0);
-        return result;
-    }
 }
